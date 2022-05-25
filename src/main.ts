26c85d517adce110d0c1d127f281fbb07d5eff3f
vipin.verma <vipin.verma@chorus.ai>
2022-05-25 10:32:27 -0400

    added cors options

--- src/main.ts
+++ src/main.ts
@@ -4,19 +4,26 @@ import { AppModule } from './app.module';
 import { configService } from './database/database.config';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, { cors: true });
   if (!configService.isProduction()) {
     const document = SwaggerModule.createDocument(
       app,
       new DocumentBuilder()
-        .setTitle('Item API')
-        .setDescription('My Item API')
+        .setTitle('Sucasa API')
+        .setDescription('List of all sucasa API')
         .build(),
     );
 
     SwaggerModule.setup('docs', app, document);
   }
-  app.enableCors();
+  const options = {
+    origin: '*',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+    preflightContinue: false,
+    optionsSuccessStatus: 204,
+    credentials: true,
+  };
+  app.enableCors(options);
   await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
