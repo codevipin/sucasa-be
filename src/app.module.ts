47f009425edb9cf2fb01699d157fae30beb3fdf7
vipin.verma <vipin.verma@chorus.ai>
2022-05-25 11:56:29 -0400

    added fe code to be served as static from nestJs

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,9 +6,14 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { configService } from './database/database.config';
 import { SpeakerModule } from './api/speaker/speaker.module';
+import { ServeStaticModule } from '@nestjs/serve-static';
+import { join } from 'path';
 @Module({
   imports: [
     TypeOrmModule.forRoot(configService.getTypeOrmConfig()),
+    ServeStaticModule.forRoot({
+      rootPath: join(__dirname, '..', 'build'),
+    }),
     PresentationModule,
     AttendeeModule,
     SpeakerModule,
